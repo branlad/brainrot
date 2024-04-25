00cf5ffcb5a5f58ffa5f03dc3f65bb78d85fe0cb
matthew <matthewtledford@gmail.com>
2024-04-25 15:09:16 -0500

    second pages functionality

--- brainrot/main.py
+++ brainrot/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/add', methods=['POST'])
+def video():
+    return render_template('video_creation.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
