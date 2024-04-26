e485bc15867e958e9c2351167acce2b719ca8378
Jacob Murrah <110310485+jmurrah@users.noreply.github.com>
2024-04-26 11:17:58 -0500

    Video improvement (#6)
    * somewhat working
    
    * change TTS, elevenLABS too quick + changes speed
    
    * WHY
    
    * kinda of working
    
    ---------
    
    Co-authored-by: jmurrah <jacob.murrah@adtran.com>

--- brainrot/main.py
+++ brainrot/main.py
@@ -1,42 +1,49 @@
 from flask import Flask, render_template, jsonify, request
 from .reddit_scraper import get_post, get_post_from_url
 from .video import get_video
+from .reddit_scraper import get_post, get_post_from_url
+from .video import get_video
 
 app = Flask(__name__)
 
-@app.route('/api/get_post', methods=['GET'])
+
+@app.route("/api/get_post", methods=["GET"])
 def api_get_post():
-    subreddit_name = request.args.get('subreddit_name', default=None, type=str)
+    subreddit_name = request.args.get("subreddit_name", default=None, type=str)
     post = get_post(subreddit_name=subreddit_name)
 
     if post:
         get_video(post)
         return jsonify(post.__dict__), 200
     else:
-        return jsonify({'error': 'No suitable post found or invalid subreddit'}), 404
+        return jsonify({"error": "No suitable post found or invalid subreddit"}), 404
 
-@app.route('/api/get_post_from_url', methods=['GET'])
+
+@app.route("/api/get_post_from_url", methods=["GET"])
 def api_get_post_from_url():
-    url = request.args.get('url', type=str)
+    url = request.args.get("url", type=str)
     post = get_post_from_url(url)
 
     if post:
         get_video(post)
         return jsonify(post.__dict__), 200
     else:
-        return jsonify({'error': 'Post not found or error accessing the URL'}), 404
+        return jsonify({"error": "Post not found or error accessing the URL"}), 404
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/add', methods=['POST'])
+
+@app.route("/add", methods=["POST"])
 def video():
-    return render_template('video_creation.html')
+    return render_template("video_creation.html")
+
 
 def main():
     app.run(debug=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- brainrot/reddit_scraper.py
+++ brainrot/reddit_scraper.py
@@ -7,10 +7,12 @@ from better_profanity import profanity
 
 SUBREDDITS = ["shortscarystories", "nosleep", "creepypasta", "horrorstories"]
 
+
 @dataclass
 class RedditPost:
     title: str
     text: str
+
 
 def get_post(subreddit_name=None):
     """Fetch and return a post from a specified subreddit, cycling through a list if none is specified."""
@@ -26,12 +28,14 @@ def get_post(subreddit_name=None):
         print(f"Accessing subreddit: {subreddit_name}")
         for post in subreddit.hot(limit=10):
             if post.is_self and not post.stickied:
+                print(f"Found post: {post.url}")
                 return post_content(post)
     except praw.exceptions.PRAWException as e:
         print(f"Error accessing subreddit or processing posts: {e}")
 
     print("No suitable text posts found.")
     return None
+
 
 def get_post_from_url(url):
     """Fetch and return a Reddit post by URL."""
@@ -42,6 +46,7 @@ def get_post_from_url(url):
     try:
         post = reddit.submission(url=url)
         if post.is_self:
+            print(f"Found post: {post.url}")
             return post_content(post)
     except praw.exceptions.PRAWException as e:
         print(f"Error accessing specific post: {e}")
@@ -50,17 +55,19 @@ def get_post_from_url(url):
 
 ########## HELPER FUNCTIONS ##########
 
+
 def setup_reddit_client():
     load_dotenv()
     try:
         return praw.Reddit(
             client_id=os.getenv("REDDIT_CLIENT_ID"),
             client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
-            user_agent=os.getenv("REDDIT_USER_AGENT")
+            user_agent=os.getenv("REDDIT_USER_AGENT"),
         )
     except Exception as e:
         print(f"Error setting up Reddit API: {e}")
         return None
+
 
 def post_content(post):
     """Censor and return the title and text of a post."""
@@ -69,28 +76,31 @@ def post_content(post):
     text = profanity.censor(post.selftext)
     return RedditPost(title=title, text=text)
 
+
 def choose_subreddit():
     subreddits = SUBREDDITS
-    index_file = 'subreddit_index.txt'
+    index_file = "subreddit_index.txt"
     current_index = get_current_index(index_file)
     update_index(index_file, current_index, len(subreddits))
     return subreddits[current_index]
 
+
 def get_current_index(file_path):
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             return int(file.read().strip())
     except FileNotFoundError:
-        with open(file_path, 'w') as file:
+        with open(file_path, "w") as file:
             file.write("0")
         return 0
     except Exception as e:
         print(f"Error reading index file: {e}")
         return 0
 
+
 def update_index(file_path, current_index, max_index):
     try:
-        with open(file_path, 'w') as file:
+        with open(file_path, "w") as file:
             file.write(str((current_index + 1) % max_index))
     except Exception as e:
         print(f"Error updating index file: {e}")
@@ -103,4 +113,3 @@ if __name__ == "__main__":
         print(post.text)
     else:
         print("Failed to retrieve a post.")
-        
