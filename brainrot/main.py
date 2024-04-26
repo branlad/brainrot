15dedd9bc4f4e4c3c9b87d26ca2c14e2c0ab35dc
jmurrah <jacobhmurrah@gmail.com>
2024-04-25 20:47:39 -0500

    somewhat working

--- brainrot/main.py
+++ brainrot/main.py
@@ -1,23 +1,28 @@
 from flask import Flask, render_template, jsonify, request
-from reddit_scraper import get_post, get_post_from_url
+from .reddit_scraper import get_post, get_post_from_url
+from .video import get_video
 
 app = Flask(__name__)
 
 @app.route('/api/get_post', methods=['GET'])
 def api_get_post():
     subreddit_name = request.args.get('subreddit_name', default=None, type=str)
-    result = get_post(subreddit_name=subreddit_name)
-    if result:
-        return jsonify(result.__dict__), 200
+    post = get_post(subreddit_name=subreddit_name)
+
+    if post:
+        get_video(post)
+        return jsonify(post.__dict__), 200
     else:
         return jsonify({'error': 'No suitable post found or invalid subreddit'}), 404
 
 @app.route('/api/get_post_from_url', methods=['GET'])
 def api_get_post_from_url():
     url = request.args.get('url', type=str)
-    result = get_post_from_url(url)
-    if result:
-        return jsonify(result.__dict__), 200
+    post = get_post_from_url(url)
+
+    if post:
+        get_video(post)
+        return jsonify(post.__dict__), 200
     else:
         return jsonify({'error': 'Post not found or error accessing the URL'}), 404
 
@@ -29,5 +34,9 @@ def index():
 def video():
     return render_template('video_creation.html')
 
-if __name__ == '__main__':
+def main():
     app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
